c6bb389d7a42705fa0621ddd88197422a9a70842
LaalShaitaan <spalriwalau@gmail.com>
2020-06-18 19:23:39 +0530

    Modularizing Code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs=`
-type Query{
-    info: String!
-}
-`
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount=links.length
 
 const resolvers={
     Query:{
-        info:()=>`This is an API of Hackernews Clone`
+        info:()=>`This is an API of Hackernews Clone`,
+        feed: () => links,
+    },
+    Mutation:{
+        post:(parents,args)=>{
+            const link={
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        }
     }
 }
 
 const server= new GraphQLServer({
-    typeDefs,
+    typeDefs:'./src/schema.graphql',
     resolvers,
 })
 server.start(()=>console.log(`Server is up and running on Port 4000`))
